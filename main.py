2976678e8233f7ae9d000e9e6bef63c6f2840679
punjordi <jspun@ualberta.ca>
2019-07-30 16:01:37 -0600

    prints

--- main.py
+++ main.py
@@ -28,6 +28,7 @@ def main():
             attack = input("You go first! Make an attack x y 0-7 \n")
             attackarray = attack.split(" ")
             sockets.connect()
+            print(tuple(int(attackarray[0]),int(attackarray[1])))
             sockets.send(tuple(int(attackarray[0]),int(attackarray[1])))
 
         #Going Second
@@ -50,6 +51,7 @@ def main():
 
         #Going First
         elif (connectedmsg == "s"):
+            print(tuple(int(attackarray[0]),int(attackarray[1])))
             sockets.send(tuple(int(attackarray[0]),int(attackarray[1])))
 
 
