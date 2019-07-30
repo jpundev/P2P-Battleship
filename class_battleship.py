dd1caf9b1d1fe36c0d788254b8180c92c7e4e2ac
yuqian5 <28016308+yuqian5@users.noreply.github.com>
2019-07-30 14:37:34 -0600

    new method added to class_battleship

--- class_battleship.py
+++ class_battleship.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+
 class BattleShip:
     def __init__(self, initINFO):
         '''
@@ -15,23 +16,29 @@ class BattleShip:
          ori is a int like 0, 1, 2, 3
         '''
 
-        #import the position of each ship from initINFO (type dict)
-        #self.ships, index0=carrier, index1=battleship, index2=cruiser, index3=submarine, index4=destoryer
+        # import the position of each ship from initINFO (type dict)
+        # self.ships, index0=carrier, index1=battleship, index2=cruiser, index3=submarine, index4=destoryer
         self.ships = []
-        self.ships.append({'pos':initINFO['carrier'][0], 'ori':initINFO['carrier'][1], 'health':5, 'sunk':False, 'hitmap':[]})
-        self.ships.append({'pos':initINFO['battleship'][0], 'ori':initINFO['battleship'][1], 'health':4, 'sunk':False, 'hitmap':[]})
-        self.ships.append({'pos':initINFO['cruiser'][0], 'ori':initINFO['cruiser'][1], 'health':3, 'sunk':False, 'hitmap':[]})
-        self.ships.append({'pos':initINFO['submarine'][0], 'ori':initINFO['submarine'][1], 'health':3, 'sunk':False, 'hitmap':[]})
-        self.ships.append({'pos':initINFO['destroyer'][0], 'ori':initINFO['destroyer'][1], 'health':2, 'sunk':False, 'hitmap':[]})
+        self.ships.append(
+            {'pos': initINFO['carrier'][0], 'ori': initINFO['carrier'][1], 'health': 5, 'sunk': False, 'hitmap': []})
+        self.ships.append(
+            {'pos': initINFO['battleship'][0], 'ori': initINFO['battleship'][1], 'health': 4, 'sunk': False,
+             'hitmap': []})
+        self.ships.append(
+            {'pos': initINFO['cruiser'][0], 'ori': initINFO['cruiser'][1], 'health': 3, 'sunk': False, 'hitmap': []})
+        self.ships.append({'pos': initINFO['submarine'][0], 'ori': initINFO['submarine'][1], 'health': 3, 'sunk': False,
+                           'hitmap': []})
+        self.ships.append({'pos': initINFO['destroyer'][0], 'ori': initINFO['destroyer'][1], 'health': 2, 'sunk': False,
+                           'hitmap': []})
 
-        #create empty board
+        # create empty board
         self.createBoard()
-        #check if ship placement is valid
+        # check if ship placement is valid
         self.checkPlacement()
-        #create hit position
+        # create hit position
         self.createHitMap()
 
-    #create hit position
+    # create hit position
     def createHitMap(self):
         for i in self.ships:
             i['hitmap'].append(i['pos'])
@@ -60,32 +67,33 @@ class BattleShip:
         for i in self.ships:
             stern = list(i['pos'])
 
-    #create a 2D numpy array, fill with '?'
+    # create a 2D numpy array, fill with '?'
     def createBoard(self):
         temp = []
         for i in range(64):
             temp.append('?')
         self.OPPboard = np.reshape(np.array(temp), (-1, 8))
 
-    #update the board according to status and pos
-    #pos is a tuple (x, y) and status may equal to 0(miss), 1(hit)
+    # update the board according to status and pos
+    # pos is a tuple (x, y) and status may equal to 0(miss), 1(hit)
     def updateBoard(self, pos, status):
         if status == 0:
             self.OPPboard[pos[0]][pos[1]] = 'O'
         else:
             self.OPPboard[pos[0]][pos[1]] = 'X'
 
-    #return 0 if no hit, return 1 for hit, return 2 for hit and sunk
+    # return 0 if no hit, return 1 for hit, return 2 for hit and sunk
     def checkAttack(self, pos):
         for i in self.ships:
             if pos in i['hitmap']:
                 i['hitmap'].remove(pos)
                 i['health'] -= 1
                 if i['health'] == 0:
-                    return 2 #hit and sunk ship
-                return 1 #hit ship
-        return 0 #no hit
+                    return 2  # hit and sunk ship
+                return 1  # hit ship
+        return 0  # no hit
 
+    # if all ship is sunk, return True
     def defeated(self):
         defeated = True
         for i in self.ships:
@@ -94,3 +102,15 @@ class BattleShip:
                 return defeated
         return defeated
 
+    #print out board with format
+    def printBoard(self):
+        for i in range(8):
+            line = "|"
+            for j in range(8):
+                line += self.OPPboard[i][j]
+                line += "|"
+            print(line)
+
+    def debug(self):
+        print(self.ships)
+        print(self.OPPboard)
