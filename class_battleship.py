9c7ff8792016dc747ead5fa5600960138cb754c2
yuqian5 <28016308+yuqian5@users.noreply.github.com>
2019-07-30 13:00:36 -0600

    edited to class_battleship

--- class_battleship.py
+++ class_battleship.py
@@ -17,18 +17,33 @@ class BattleShip:
         '''
 
         #import the position of each ship from initINFO (type dict)
-        self.ships = {}
-        self.ships['carrier'] = {'pos':initINFO['carrier'][0], 'ori':initINFO['carrier'][1], 'health':5, 'sunk':False}
-        self.ships['battleship'] = {'pos':initINFO['battleship'][0], 'ori':initINFO['battleship'][1], 'health':4, 'sunk':False}
-        self.ships['cruiser'] = {'pos':initINFO['cruiser'][0], 'ori':initINFO['cruiser'][1], 'health':3, 'sunk':False}
-        self.ships['submarine'] = {'pos':initINFO['submarine'][0], 'ori':initINFO['submarine'][1], 'health':3, 'sunk':False}
-        self.ships['destroyer'] = {'pos':initINFO['destroyer'][0], 'ori':initINFO['destroyer'][1], 'health':2, 'sunk':False}
+        #self.ships, index0=carrier, index1=battleship, index2=cruiser, index3=submarine, index4=destoryer
+        self.ships = []
+        self.ships.append({'pos':initINFO['carrier'][0], 'ori':initINFO['carrier'][1], 'health':5, 'sunk':False, 'hitmap':[]})
+        self.ships.append({'pos':initINFO['battleship'][0], 'ori':initINFO['battleship'][1], 'health':4, 'sunk':False, 'hitmap':[]})
+        self.ships.append({'pos':initINFO['cruiser'][0], 'ori':initINFO['cruiser'][1], 'health':3, 'sunk':False, 'hitmap':[]})
+        self.ships.append({'pos':initINFO['submarine'][0], 'ori':initINFO['submarine'][1], 'health':3, 'sunk':False, 'hitmap':[]})
+        self.ships.append({'pos':initINFO['destroyer'][0], 'ori':initINFO['destroyer'][1], 'health':2, 'sunk':False, 'hitmap':[]})
 
         #create empty board
         self.createBoard()
+        self.createHitMap()
 
+    #
     def createHitMap(self):
-        pass
+        for i in self.ships:
+            i['hitmap'].append(i['pos'])
+            pos_temp = [i['pos'][0], i['pos'][1]]
+            if i['ori'] == 0:
+                for j in range(i['health']):
+                    pos_temp[0] += 1
+                    i['hitmap'].append(i['pos'])
+            elif i['ori'] == 1:
+                pass
+            elif i['ori'] == 2:
+                pass
+            elif i['ori'] == 3:
+                pass
 
     def checkPlacement(self):
         pass
