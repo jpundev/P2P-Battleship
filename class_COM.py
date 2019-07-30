57c13c6516d0d1681e8803e67906d58413d142db
punjordi <jspun@ualberta.ca>
2019-07-30 13:11:46 -0600

    added socket functionality for the beginning of the game

--- class_COM.py
+++ class_COM.py
@@ -1,22 +1,35 @@
 import socket, pickle
+import netifaces as ni
 
 class COM:
     def __init__(self, OPPip, txport, rxport):
+        self.OPPip = OPPip
+        self.txport = txport
+        self.rxport = rxport
         self.tx = socket.socket()
-        self.tx.connect((str(OPPip), txport))
         self.rx = socket.socket()
-        self.rx.bind((str(OPPip), rxport))
+        self.ip = self.selfip()
+        self.rx.bind((str(self.ip), int(rxport)))
 
     def stop(self):
         self.rx.close()
         self.tx.close()
 
     def send(self,msg):
-        msg_b = pickle.loads(msg)
+        msg_b = pickle.dumps(msg)
         self.tx.send(msg_b)
 
     def recieve(self):
+        self.rx.listen()
         c, addr = self.rx.accept()
         msg_b = c.recv(1024)
-        msg = pickle.dumps(msg_b)
+        msg = pickle.loads(msg_b)
         return msg
+
+    def connect(self):
+        self.tx.connect((str(self.OPPip), int(self.txport)))
+
+    def selfip(self):
+        ip = ni.ifaddresses('en0')[ni.AF_INET][0]['addr']
+        return ip
+
--- main.py
+++ main.py
@@ -1,4 +1,77 @@
-import class_battleship
+from class_battleship import BattleShip
+from class_COM import COM
+import random 
+
+def main():
+    #Gether Inputs for Socket usage and connection
+    input("Welcome To Our BattleShip Game, Press Any Button to Continue...\n")
+    OPPip = input("Enter Your opponents IP..\n")
+    txport = input("Enter Transmitting port..\n")
+    rxport = input("Enter Receiving Port...\n")
+    host = input("Are you the Host? (y/n) \n")
+    sockets = COM(OPPip,txport,rxport)
+
+    #Is Host
+    if(host == "y"):
+        print("Waiting for Player 2 to Connect..\n")
+        connectedmsg = sockets.recieve()
+        print("Player 2 is.." + connectedmsg)
+        #Going First
+        if(random.randint(1,2)==1):
+            sockets.connect()
+            sockets.send("f")
+        #Going Second
+        else:
+            sockets.connect()
+            sockets.send("s")
+
+
+    #is not host
+    else:
+        print("Connecting to Host..\n")
+        sockets.connect()
+        sockets.send("Connected")
+        connectedmsg = sockets.recieve()
+        #Going Second
+        if(connectedmsg == "f"):
+            
+
+        #Going First
+        elif (connectedmsg == "s"):
+
+
+
+    
+        
+
+    
+    
+    
+
+
+
+
+    return
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 if __name__ == '__main__':
+    main()
     pass
