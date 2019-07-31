3a558bb50668b5c9e65e1fa49177e46dfe93b81a
yuqian5 <28016308+yuqian5@users.noreply.github.com>
2019-07-30 21:39:34 -0600

    interface beautified, code optimized and refactored

--- class_battleship.py
+++ class_battleship.py
@@ -1,43 +1,63 @@
 import numpy as np
+from os import system as sys
 
 
 class BattleShip:
     def __init__(self, initINFO):
         '''
-        :param initINFO:
-        initINFO is a dictionary formatted as
-        {'carrier':[pos,ori],
-         'battleship':[pos,ori],
-         'cruiser':[pos,ori],
-         'submarine':[pos,ori],
-         'destroyer':[pos,ori]}
+        initINFO is a dictionary formatted as:
+        {
+            'carrier':[pos,ori],
+            'battleship':[pos,ori],
+            'cruiser':[pos,ori],
+            'submarine':[pos,ori],
+            'destroyer':[pos,ori]
+         }
 
-         pos is a tuple (x,y)
+         pos is a tuple (row num,column num), it signifies as the bow of the ship
          ori is a int like 0, 1, 2, 3
+            0 means the stern of the ship is to east of the bow
+            1 means the stern of the ship is to south of the bow
+            2 means the stern of the ship is to west of the bow
+            3 means the stern of the ship is to north of the bow
         '''
 
-        #variable contains all possible hit location, used for checking placement overlap
+        # variable contains all possible hit location, used for checking placement overlap
         self.globalHitMap = []
 
-        #indicate if it's your turn or not
+        # indicate if it's your turn or not
         self.turn = None
 
-        #Opponent's board
-        self.OPPboard = None
+        # Opponent's board
+        self.guessBoard = None
 
         # import the position of each ship from initINFO (type dict)
         # self.ships, index0=carrier, index1=battleship, index2=cruiser, index3=submarine, index4=destoryer
         self.ships = []
-        self.ships.append(
-            {'pos': initINFO['carrier'][0], 'ori': initINFO['carrier'][1], 'health': 5, 'sunk': False, 'hitmap': []})
-        self.ships.append(
-            {'pos': initINFO['battleship'][0], 'ori': initINFO['battleship'][1], 'health': 4, 'sunk': False,
-             'hitmap': []})
-        self.ships.append(
-            {'pos': initINFO['cruiser'][0], 'ori': initINFO['cruiser'][1], 'health': 3, 'sunk': False, 'hitmap': []})
-        self.ships.append({'pos': initINFO['submarine'][0], 'ori': initINFO['submarine'][1], 'health': 3, 'sunk': False,
+        self.ships.append({'pos': initINFO['carrier'][0],
+                           'ori': initINFO['carrier'][1],
+                           'health': 5,
+                           'sunk': False,
                            'hitmap': []})
-        self.ships.append({'pos': initINFO['destroyer'][0], 'ori': initINFO['destroyer'][1], 'health': 2, 'sunk': False,
+        self.ships.append({'pos': initINFO['battleship'][0],
+                           'ori': initINFO['battleship'][1],
+                           'health': 4,
+                           'sunk': False,
+                           'hitmap': []})
+        self.ships.append({'pos': initINFO['cruiser'][0],
+                           'ori': initINFO['cruiser'][1],
+                           'health': 3,
+                           'sunk': False,
+                           'hitmap': []})
+        self.ships.append({'pos': initINFO['submarine'][0],
+                           'ori': initINFO['submarine'][1],
+                           'health': 3,
+                           'sunk': False,
+                           'hitmap': []})
+        self.ships.append({'pos': initINFO['destroyer'][0],
+                           'ori': initINFO['destroyer'][1],
+                           'health': 2,
+                           'sunk': False,
                            'hitmap': []})
 
         # create empty board
@@ -51,56 +71,43 @@ class BattleShip:
             i['hitmap'].append(i['pos'])
             pos_temp = list(i['pos'])
             if i['ori'] == 0:
-                for j in range(i['health']-1):
+                for j in range(i['health'] - 1):
                     pos_temp[1] += 1
-                    i['hitmap'].append(tuple(pos_temp))
-                    if not self.placementOverlap(tuple(pos_temp)) and self.indexInRange(tuple(pos_temp)):
-                        self.globalHitMap.append(tuple(pos_temp))
-                    else:
-                        print("error in ship placement, make sure the ship don't overlap or go outside of the board")
-                        exit(1)
-
+                    self.placementCheck(pos_temp, i)
             elif i['ori'] == 1:
-                for j in range(i['health']-1):
+                for j in range(i['health'] - 1):
                     pos_temp[0] += 1
-                    i['hitmap'].append(tuple(pos_temp))
-                    if not self.placementOverlap(tuple(pos_temp)) and self.indexInRange(tuple(pos_temp)):
-                        self.globalHitMap.append(tuple(pos_temp))
-                    else:
-                        print("error in ship placement, make sure the ship don't overlap or go outside of the board")
-                        exit(1)
-
+                    self.placementCheck(pos_temp, i)
             elif i['ori'] == 2:
-                for j in range(i['health']-1):
+                for j in range(i['health'] - 1):
                     pos_temp[1] -= 1
-                    i['hitmap'].append(tuple(pos_temp))
-                    if not self.placementOverlap(tuple(pos_temp)) and self.indexInRange(tuple(pos_temp)):
-                        self.globalHitMap.append(tuple(pos_temp))
-                    else:
-                        print("error in ship placement, make sure the ship don't overlap or go outside of the board")
-                        exit(1)
-
+                    self.placementCheck(pos_temp, i)
             elif i['ori'] == 3:
-                for j in range(i['health']-1):
+                for j in range(i['health'] - 1):
                     pos_temp[0] -= 1
-                    i['hitmap'].append(tuple(pos_temp))
-                    if not self.placementOverlap(tuple(pos_temp)) and self.indexInRange(tuple(pos_temp)):
-                        self.globalHitMap.append(tuple(pos_temp))
-                    else:
-                        print("error in ship placement, make sure the ship don't overlap or go outside of the board")
-                        exit(1)
-
+                    self.placementCheck(pos_temp, i)
             else:
                 print("invalid orientation input")
                 exit(1)
 
-    #return true if ship is placed outside of the board
+    # called in createHitMap(), it uses indexInRange() and placementOverlap to check the validity of the placement
+    def placementCheck(self, pos_temp, i):
+        i['hitmap'].append(tuple(pos_temp))
+        # if the placement is valid, add that to the self.globalHitMap and continue
+        # if not valid, print error and exit
+        if not self.placementOverlap(tuple(pos_temp)) and self.indexInRange(tuple(pos_temp)):
+            self.globalHitMap.append(tuple(pos_temp))
+        else:
+            print("error in ship placement, make sure the ships don't overlap or go outside of the board")
+            exit(1)
+
+    # return true if ship is placed outside of the board
     def indexInRange(self, pos):
-        if pos[0] >= 0 and pos[0] <= 7 and pos[1] >= 0 and pos[1] <= 7:
+        if 0 <= pos[0] <= 7 and 0 <= pos[1] <= 7:
             return True
         return False
 
-    #return true if 2 ships overlaps
+    # return true if 2 ships overlaps
     def placementOverlap(self, pos):
         if pos in self.globalHitMap:
             return True
@@ -111,17 +118,24 @@ class BattleShip:
         temp = []
         for i in range(64):
             temp.append('?')
-        self.OPPboard = np.reshape(np.array(temp), (-1, 8))
+        self.guessBoard = np.reshape(np.array(temp), (-1, 8))
 
     # update the board according to status and pos
-    # pos is a tuple (x, y) and status may equal to 0(miss), 1(hit)
+    # clear window, print message according to status, print board
+    # pos is a tuple (row num, col num) and status may equal to 0(miss), 1(hit)
+    # 'X' means a hit 'O' means a miss
     def updateBoard(self, pos, status):
+        sys("Clear")
         if status == 0:
-            self.OPPboard[pos[0]][pos[1]] = 'O'
+            self.guessBoard[pos[0]][pos[1]] = 'O'
+            print("Oops, you missed!")
         else:
-            self.OPPboard[pos[0]][pos[1]] = 'X'
-        print("kerry sucks")
-        self.printBoard()
+            self.guessBoard[pos[0]][pos[1]] = 'X'
+            if status == 1:
+                print("Hooray!!! You hit something")
+            else:
+                print("You just sunk a ship! Flex tape can't fix that!")
+        self.printGuessBoard()
 
     # return 0 if no hit, return 1 for hit, return 2 for hit and sunk
     def checkAttack(self, pos):
@@ -136,18 +150,19 @@ class BattleShip:
 
     # if all ship is sunk, return True
     def defeated(self):
-        defeated = True
         for i in self.ships:
             if i['sunk'] is False:
-                defeated = False
-                return defeated
-        return defeated
+                return False
+        return True
 
-    #print out board with format
-    def printBoard(self):
+    # print out board with format
+    def printGuessBoard(self):
+        print("   0 1 2 3 4 5 6 7 ")
+        rowIndex = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'E']
         for i in range(8):
-            line = "|"
+            line = rowIndex[i] + " |"
             for j in range(8):
-                line += self.OPPboard[i][j]
+                line += self.guessBoard[i][j]
                 line += "|"
             print(line)
+
